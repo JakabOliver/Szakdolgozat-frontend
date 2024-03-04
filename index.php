5311b434aadc007de96cddeef212759190d1b2db
Olivér Jakab <oliver.jakab.r@gmail.com>
2024-03-04 20:25:36 +0100

    2

--- index.php
+++ index.php
@@ -7,5 +7,5 @@ require_once 'vendor/autoload.php';
 $path = substr($_SERVER['REQUEST_URI'], 1);
 $loader = new FilesystemLoader('templates');
 $twig = new Environment($loader);
-$template = $twig->load($path);
+$template = $twig->load($path.'.twig');
 echo $template->render(['additionalJS' => 'assets/js/tracker.js', 'go' => 'here']);
